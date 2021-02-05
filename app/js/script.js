f1a3e30f6d58c0d5a8c7e368b3180548d45549f8
abullatama <abullatama@gmail.com>
2021-02-05 16:39:01 +0700

    animated mobile menu with js

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,24 @@
 //Hamburger
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
+const toggle = document.querySelector(".header__toggle");
 
 btnHamburger.addEventListener("click", function () {
-  if (btnHamburger.classList.contains("open")) {
-    btnHamburger.classList.remove("open");
+  if (header.classList.contains("open")) {
+    toggle.classList.add("fade-out");
+    toggle.classList.remove("fade-in");
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    overlay.classList.add("fade-out");
+    overlay.classList.remove("fade-in");
   } else {
-    btnHamburger.classList.add("open");
+    toggle.classList.add("fade-in");
+    toggle.classList.remove("fade-out");
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    overlay.classList.add("fade-in");
+    overlay.classList.remove("fade-out");
   }
 });
