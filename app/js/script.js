4c9688b69c943714485afd3e809cb75379540c0e
abullatama <abullatama@gmail.com>
2021-02-05 13:12:45 +0700

    creating the hamburger menu functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,10 @@
-console.log("test");
-const abul = () => {
-  if ((1 = 1)) {
-    1 = 1;
+//Hamburger
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", function () {
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
   }
-};
+});
